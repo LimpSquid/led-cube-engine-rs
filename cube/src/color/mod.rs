0933b18d21701dadf4a6a49b0c2fe7e8c1683bea
LimpSquid <git@limpsquid.nl>
2022-10-19 23:15:59 +0200

    Work in progress.

--- cube/src/color/mod.rs
+++ cube/src/color/mod.rs
@@ -87,6 +87,7 @@ macro_rules! count {
 macro_rules! impl_named_colors {
     ($($name:ident = $color:expr),+) => {
         lazy_static! {
+            // Invariant: named colors are unique
             static ref NAMED_COLORS: [(String, Color); count!($($name)+)] = [
                 $((String::from(stringify!($name)).to_lowercase(), $name),)+
             ];
